386dba320d57b16fb00e64e7b5a5b3b53cbea624
Aji Furqon <furqonajiy@caditglobal.com>
2022-01-10 18:03:23 +0700

    Update App.css

--- reactjsfrontend/src/App.js
+++ reactjsfrontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css'
 
 import { Footer, Blog, Possibility, Features, WhoFurqonajiy, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
